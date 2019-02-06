5dc80e0067a81fe1d753cde3bd264af8fb02ae46
gallenliu <gallenliu@hotmail.com>
2019-02-05 21:21:25 -0500

    change card image size

---
+++ src/biz/game.js
@@ -0,0 +1,12 @@
+import './card';
+
+class Game {
+    constructor(){
+        this.startTime = null;
+        this.endTime = null;
+        this.users = [];
+        this.rounds = [];
+        this.state = {};
+    }
+
+}
