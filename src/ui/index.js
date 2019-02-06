3fccb8489146deaa024ce33a9976bb1a37a519f1
Jonathan Liu <Blackmagic919@gallens-Air.fios-router.home>
2019-02-05 21:06:03 -0500

    Added and Renamed all the cards

--- src/ui/index.js
+++ src/ui/index.js
@@ -13,14 +13,31 @@ var config = {
 
 var game = new Phaser.Game(config);
 
-function preload ()
+function loadCards(that){
+    let ret = [];
+    const suits = 'SCHD';
+    const faces = '3456789JQK';
+    for (let s = 0; s < suits.length; s++){
+        for (let f = 0; f < faces.length; f++){
+            that.load.image(faces[f] + suits[s] , 'assets/cards/' + faces[f] + suits[s] + '.png');
+        }
+        that.load.image('10' + suits[s], 'assets/cards/10' + suits[s] + '.png');
+    }
+    that.load.image('AS', 'assets/cards/AS.png');
+    that.load.image('AC' , 'assets/cards/AC.png');
+    that.load.image('AH', 'assets/cards/AH.png');
+    that.load.image('2H', 'assets/cards/2H.png');  
+}
+
+function preload()
 {
     this.load.image('logo', 'assets/logo.png');
+    loadCards(this)
 }
 
 function create ()
 {
-    var logo = this.add.image(400, 150, 'logo');
+    var logo = this.add.image(400, 150, 'AC');
 
     this.tweens.add({
         targets: logo,
